947de80747febb37e4deeade4fffec3418f76ba3
evtrnist <evagrandet@ro.ru>
2023-11-30 22:52:18 +0400

    adds axios

--- src/air-q.service.ts
+++ src/air-q.service.ts
@@ -1,18 +1,18 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 const URL = `http://api.waqi.info/feed/@${process.env.STATION_ID}/?token=${process.env.API_TOKEN}`;
 
 @Injectable()
 export class AirQService {
+  constructor(private readonly httpService: HttpService) {}
   public async onMessage() {
     try {
-      const response = await fetch(URL);
+      let data;
 
-      if (!response.ok) {
-        throw new Error(`Fetch failed with status ${response.status}`);
-      }
-
-      const data = await response.json();
+      this.httpService.get(URL).subscribe((res) => {
+        data = res;
+      });
 
       return data;
     } catch (error) {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegrafModule } from 'nestjs-telegraf';
@@ -6,6 +7,7 @@ import { AirQService } from './air-q.service';
 
 @Module({
   imports: [
+    HttpModule,
     TelegrafModule.forRoot({
       token: process.env.BOT_TOKEN,
     }),
