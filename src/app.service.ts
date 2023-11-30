a7254110555e6b38a490f85c0f7edca5a5b7bb1b
evtrnist <evagrandet@ro.ru>
2023-11-30 22:16:36 +0400

    adds some commands

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Hears, On, Start, Update } from 'nestjs-telegraf';
+import { Context } from 'vm';
 
+@Update()
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getData(): { message: string } {
+    return { message: 'Welcome to server!' };
+  }
+
+  @Start()
+  async startCommand(ctx: Context) {
+    await ctx.reply('Сейчас мы узнаем откуда готовилось нагазение');
+  }
+
+  @On('sticker')
+  async onSticker(ctx: Context) {
+    await ctx.reply('Сомнительно, но окэй');
+  }
+
+  @Hears('го')
+  async hearsHi(ctx: Context) {
+    await ctx.reply('Hey there');
   }
 }
