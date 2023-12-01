d1de697231c8cfd6f7c003abaeb679e860a64aa2
evtrnist <evagrandet@ro.ru>
2023-12-01 09:29:23 +0400

    change api url

--- src/air-q.service.ts
+++ src/air-q.service.ts
@@ -1,14 +1,14 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
-const URL = `https://api.waqi.info/feed/${process.env.STATION_ID}/?token=${process.env.API_TOKEN}`;
-
+const URL = `https://api.waqi.info/feed/@${process.env.STATION_ID}/?token=${process.env.API_TOKEN}`;
+const OTHER_URL = `https://data.sensor.community/airrohr/v1/sensor/${process.env.STATION_ID}`
 @Injectable()
 export class AirQService {
   constructor(private readonly httpService: HttpService) {}
   public async onMessage() {
     try {
-      const response = await fetch(URL, {
+      const response = await fetch(OTHER_URL, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
