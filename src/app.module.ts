98aaa12e911d292b18497791f0c7401a6e5eda24
evtrnist <evagrandet@ro.ru>
2023-12-07 21:54:42 +0400

    updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { AirQService } from './air-q.service';
-// import LocalSession from 'telegraf-session-local';
-import { SessionState } from './session-state';
+import { PrismaModule } from 'prisma/prisma.module';
 import { StartCommandService } from './start-command.service';
-
-import LocalSession = require('telegraf-session-local');
-
-const localSession = new LocalSession<SessionState>({
-  database: 'session_db.json',
-});
 
 @Module({
   imports: [
     HttpModule,
+    PrismaModule,
     ScheduleModule.forRoot(),
     TelegrafModule.forRoot({
       token: process.env.BOT_TOKEN,
-      middlewares: [localSession.middleware()],
     }),
   ],
   controllers: [AppController],
