160f907414e3e445a24644f061a61cc652e61643
evtrnist <evagrandet@ro.ru>
2023-12-24 21:49:54 +0400

    small fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { HttpModule } from '@nestjs/axios';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { AirQService } from './air-q.service';
@@ -14,7 +13,6 @@ import { AirQService } from './air-q.service';
       token: process.env.BOT_TOKEN,
     }),
   ],
-  controllers: [AppController],
   providers: [AppService, AirQService],
 })
 export class AppModule {}
